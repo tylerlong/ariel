e581c5a852a2e5b609d1115757551f0e98023607
Tyler Long <tyler4long@gmail.com>
2018-03-20 13:34:35 +0800

    Refactor code

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 const fs = require('fs')
 
-const { rectSize, createBody } = require('./src/utils')
-const { textY, fontFamily, fontSize, lineHeight } = require('./src/constants')
+const { createBody, drawTextRect } = require('./src/utils')
 
 const body = createBody()
 const svg = body.append('svg').attr('xmlns', 'http://www.w3.org/2000/svg')
   .attr('width', 1500).attr('height', 500)
 const g = svg.append('g')
 
-const text = '<jeep did Google is grea! 快讯：习近平在十三届全国人大一次会议上发表重要讲话。 Browse the commercial free fonts classified as monospaced.>'
+const text = '<AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz 快讯：习近平在十三届全国人大一次会议上发表重要讲话。 Browse the commercial free fonts classified as monospaced.>'
 
-const size = rectSize(text)
-g.append('rect').attr('x', 0).attr('y', 0)
-  .attr('width', size.width).attr('height', size.height)
-  .attr('fill', 'white').attr('stroke', 'black')
-
-g.append('text')
-  .attr('x', 0)
-  .attr('y', textY)
-  .attr('font-family', fontFamily)
-  .attr('font-size', fontSize)
-  .attr('line-height', lineHeight)
-  .text(text)
+drawTextRect(g, text, 100, 100)
 
 fs.writeFileSync('test.svg', body.html())
--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ const isFullwidthCodePoint = require('is-fullwidth-code-point')
 const jsdom = require('jsdom')
 const d3 = require('d3')
 
-const { asciiCharWidth, cjkCharWidth, charHeight } = require('./constants')
+const { asciiCharWidth, cjkCharWidth, charHeight, textY, fontFamily, fontSize, lineHeight } = require('./constants')
 
 const charWidth = c => {
   return isFullwidthCodePoint(c.codePointAt()) ? cjkCharWidth : asciiCharWidth
@@ -23,8 +23,24 @@ const rectSize = text => {
   return { width: textWidth + padding * 2, height: charHeight + padding * 2 }
 }
 
+const drawTextRect = (g, text, x, y) => {
+  const size = rectSize(text)
+  g.append('rect').attr('x', x).attr('y', x)
+    .attr('width', size.width).attr('height', size.height)
+    .attr('fill', 'white').attr('stroke', 'black')
+
+  g.append('text')
+    .attr('x', x)
+    .attr('y', x + textY)
+    .attr('font-family', fontFamily)
+    .attr('font-size', fontSize)
+    .attr('line-height', lineHeight)
+    .text(text)
+}
+
 module.exports = {
   charWidth,
   createBody,
-  rectSize
+  rectSize,
+  drawTextRect
 }
