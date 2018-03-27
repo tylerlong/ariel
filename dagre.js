1551803a3939dfad004c70f6d833f11ada3f2f70
Tyler Long <tyler4long@gmail.com>
2018-03-27 16:26:21 +0800

    What if minX / minY is not 0

--- dagre.js
+++ dagre.js
@@ -9,27 +9,37 @@ const { rectWidth, rectHeight, padding } = require('./src/constants')
 const graph = new graphlib.Graph()
 
 // Set an object for the graph label
-graph.setGraph({rankdir: 'BT'})
+graph.setGraph({rankdir: 'TB'})
 
 // Default to assigning a new object as a label for each new edge.
 graph.setDefaultEdgeLabel(function () { return {} })
 
+// A-->B
+// A-->C
+// A-->Z
+// A-->Y
+// B-->Y
+// Z-->Y
+// Y-->A
+
 // Add nodes to the graph. The first argument is the node id. The second is
 // metadata about the node. In this case we're going to add labels to each of
 // our nodes.
-graph.setNode('kspacey', { label: 'Kevin Spacey', width: rectWidth, height: rectHeight })
-graph.setNode('swilliams', { label: 'Saul Williams', width: rectWidth, height: rectHeight })
-graph.setNode('bpitt', { label: 'Brad 👨‍👨‍👧 🌺 Pitt', width: rectWidth, height: rectHeight })
-graph.setNode('hford', { label: 'Harrison Ford', width: rectWidth, height: rectHeight })
-graph.setNode('lwilson', { label: 'Luke Wilson', width: rectWidth, height: rectHeight })
-graph.setNode('kbacon', { label: 'Kevin Bacon', width: rectWidth, height: rectHeight })
+graph.setNode('A', { label: 'A', width: rectWidth, height: rectHeight })
+graph.setNode('B', { label: 'B', width: rectWidth, height: rectHeight })
+graph.setNode('C', { label: 'C', width: rectWidth, height: rectHeight })
+graph.setNode('Z', { label: 'Z', width: rectWidth, height: rectHeight })
+graph.setNode('Y', { label: 'Y', width: rectWidth, height: rectHeight })
+// graph.setNode('kbacon', { label: 'Kevin Bacon', width: rectWidth, height: rectHeight })
 
 // Add edges to the graph.
-graph.setEdge('kspacey', 'swilliams')
-graph.setEdge('swilliams', 'kbacon')
-graph.setEdge('bpitt', 'kbacon')
-graph.setEdge('hford', 'lwilson')
-graph.setEdge('lwilson', 'kbacon')
+graph.setEdge('A', 'B')
+graph.setEdge('A', 'C')
+graph.setEdge('A', 'Z')
+graph.setEdge('A', 'Y')
+graph.setEdge('B', 'Y')
+graph.setEdge('Z', 'Y')
+graph.setEdge('Y', 'A')
 
 dagre.layout(graph)
 
@@ -66,6 +76,6 @@ graph.edges().forEach(e => {
 console.log(minX, minY, maxX, maxY)
 
 svg.attr('width', maxX - minX + padding * 2).attr('height', maxY - minY + padding * 2)
-g.attr('transform', `translate(${padding}, ${padding})`)
+g.attr('transform', `translate(${padding - minX}, ${padding - minY})`)
 
 fs.writeFileSync('test.svg', body.html())
