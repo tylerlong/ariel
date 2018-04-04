903e2d388745c067b699822ecd4eec27bb6d2fbb
Tyler Long <tyler4long@gmail.com>
2018-04-04 17:44:47 +0800

    Move method

--- src/utils.js
+++ src/utils.js
@@ -77,7 +77,7 @@ const intersect = (node, point) => {
     case 'ellipse':
       return intersectEllipse(node, node.width / 2.0, node.height / 2.0, point)
     default:
-      return intersectRect(node, point)
+      return Rect.intersect(node, point)
   }
 }
 
@@ -107,37 +107,6 @@ const intersectEllipse = (node, rx, ry, point) => {
   }
 
   return {x: cx + dx, y: cy + dy}
-}
-
-const intersectRect = (node, point) => {
-  const x = node.x
-  const y = node.y
-
-  // Rectangle intersection algorithm from:
-  // http://math.stackexchange.com/questions/108113/find-edge-between-two-boxes
-  const dx = point.x - x
-  const dy = point.y - y
-  let w = node.width / 2
-  let h = node.height / 2
-
-  let sx, sy
-  if (Math.abs(dy) * w > Math.abs(dx) * h) {
-    // Intersection is top or bottom of rect.
-    if (dy < 0) {
-      h = -h
-    }
-    sx = dy === 0 ? 0 : h * dx / dy
-    sy = h
-  } else {
-    // Intersection is left or right of rect.
-    if (dx < 0) {
-      w = -w
-    }
-    sx = w
-    sy = dx === 0 ? 0 : w * dy / dx
-  }
-
-  return {x: x + sx, y: y + sy}
 }
 
 module.exports = {
