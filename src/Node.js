42b1625d800a500828535f0956ef57db37e99f8b
Tyler Long <tyler4long@gmail.com>
2018-04-06 18:35:21 +0800

    Support empty lines in label

--- src/Node.js
+++ src/Node.js
@@ -22,20 +22,48 @@ class Node {
       R.map(R.trim)
     )(this.label)
     if (lines.length % 2 === 0) { // even lines, for example: 4
+      let emptyLineHeight = -lineHeight / 2
       R.forEach(i => {
-        text.append('tspan').attr('x', '50%').attr('dy', i === Math.floor(lines.length / 2) - 1 ? -lineHeight / 2 : -lineHeight).attr('alignment-baseline', 'central').text(lines[i])
+        if (R.isEmpty(lines[i])) {
+          emptyLineHeight += lineHeight
+        } else {
+          text.append('tspan').attr('x', '50%').attr('dy', -lineHeight - emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
+          emptyLineHeight = 0
+        }
       }, R.reverse(R.range(0, Math.floor(lines.length / 2))))
       text.append('tspan').attr('x', '50%').attr('y', '50%').attr('alignment-baseline', 'central').attr('visibility', 'hidden').text('.')
+      emptyLineHeight = -lineHeight / 2
       R.forEach(i => {
-        text.append('tspan').attr('x', '50%').attr('dy', i === Math.floor(lines.length / 2) ? lineHeight / 2 : lineHeight).attr('alignment-baseline', 'central').text(lines[i])
+        if (R.isEmpty(lines[i])) {
+          emptyLineHeight += lineHeight
+        } else {
+          text.append('tspan').attr('x', '50%').attr('dy', lineHeight + emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
+          emptyLineHeight = 0
+        }
       }, R.range(Math.floor(lines.length / 2), lines.length))
     } else { // odd lines, for example: 5
+      let emptyLineHeight = 0
       R.forEach(i => {
-        text.append('tspan').attr('x', '50%').attr('dy', -lineHeight).attr('alignment-baseline', 'central').text(lines[i])
+        if (R.isEmpty(lines[i])) {
+          emptyLineHeight += lineHeight
+        } else {
+          text.append('tspan').attr('x', '50%').attr('dy', -lineHeight - emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
+          emptyLineHeight = 0
+        }
       }, R.reverse(R.range(0, Math.floor(lines.length / 2))))
-      text.append('tspan').attr('x', '50%').attr('y', '50%').attr('alignment-baseline', 'central').text(lines[Math.floor(lines.length / 2)])
+      if (R.isEmpty(lines[Math.floor(lines.length / 2)])) {
+        text.append('tspan').attr('x', '50%').attr('y', '50%').attr('alignment-baseline', 'central').attr('visibility', 'hidden').text('.')
+      } else {
+        text.append('tspan').attr('x', '50%').attr('y', '50%').attr('alignment-baseline', 'central').text(lines[Math.floor(lines.length / 2)])
+      }
+      emptyLineHeight = 0
       R.forEach(i => {
-        text.append('tspan').attr('x', '50%').attr('dy', lineHeight).attr('alignment-baseline', 'central').text(lines[i])
+        if (R.isEmpty(lines[i])) {
+          emptyLineHeight += lineHeight
+        } else {
+          text.append('tspan').attr('x', '50%').attr('dy', lineHeight + emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
+          emptyLineHeight = 0
+        }
       }, R.range(Math.floor(lines.length / 2) + 1, lines.length))
     }
   }
