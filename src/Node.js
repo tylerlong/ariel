58f335c40bef5ef556c555abe476f9399ab5aa6a
Tyler Long <tyler4long@gmail.com>
2018-04-06 18:42:41 +0800

    Create class Label

--- src/Node.js
+++ src/Node.js
@@ -1,9 +1,7 @@
-import * as R from 'ramda'
-
 import Rect from './Rect'
 import Ellipse from './Ellipse'
 import Circle from './Circle'
-import { lineHeight } from './constants'
+import Label from './Label'
 
 class Node {
   constructor (x, y, w, h, shape, label) {
@@ -13,59 +11,6 @@ class Node {
     this.h = h
     this.shape = shape
     this.label = label
-  }
-
-  drawLabel (svg) {
-    const text = svg.append('text').attr('x', '50%').attr('y', '50%').attr('fill', 'black').attr('text-anchor', 'middle').attr('dominant-baseline', 'central')
-    const lines = R.pipe(
-      R.split('\n'),
-      R.map(R.trim)
-    )(this.label)
-    if (lines.length % 2 === 0) { // even lines, for example: 4
-      let emptyLineHeight = -lineHeight / 2
-      R.forEach(i => {
-        if (R.isEmpty(lines[i])) {
-          emptyLineHeight += lineHeight
-        } else {
-          text.append('tspan').attr('x', '50%').attr('dy', -lineHeight - emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
-          emptyLineHeight = 0
-        }
-      }, R.reverse(R.range(0, Math.floor(lines.length / 2))))
-      text.append('tspan').attr('x', '50%').attr('y', '50%').attr('alignment-baseline', 'central').attr('visibility', 'hidden').text('.')
-      emptyLineHeight = -lineHeight / 2
-      R.forEach(i => {
-        if (R.isEmpty(lines[i])) {
-          emptyLineHeight += lineHeight
-        } else {
-          text.append('tspan').attr('x', '50%').attr('dy', lineHeight + emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
-          emptyLineHeight = 0
-        }
-      }, R.range(Math.floor(lines.length / 2), lines.length))
-    } else { // odd lines, for example: 5
-      let emptyLineHeight = 0
-      R.forEach(i => {
-        if (R.isEmpty(lines[i])) {
-          emptyLineHeight += lineHeight
-        } else {
-          text.append('tspan').attr('x', '50%').attr('dy', -lineHeight - emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
-          emptyLineHeight = 0
-        }
-      }, R.reverse(R.range(0, Math.floor(lines.length / 2))))
-      if (R.isEmpty(lines[Math.floor(lines.length / 2)])) {
-        text.append('tspan').attr('x', '50%').attr('y', '50%').attr('alignment-baseline', 'central').attr('visibility', 'hidden').text('.')
-      } else {
-        text.append('tspan').attr('x', '50%').attr('y', '50%').attr('alignment-baseline', 'central').text(lines[Math.floor(lines.length / 2)])
-      }
-      emptyLineHeight = 0
-      R.forEach(i => {
-        if (R.isEmpty(lines[i])) {
-          emptyLineHeight += lineHeight
-        } else {
-          text.append('tspan').attr('x', '50%').attr('dy', lineHeight + emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
-          emptyLineHeight = 0
-        }
-      }, R.range(Math.floor(lines.length / 2) + 1, lines.length))
-    }
   }
 
   draw (g) {
@@ -82,7 +27,7 @@ class Node {
     } else if (this.shape === 'ellipse') {
       new Ellipse(this.w / 2, this.h / 2, this.w / 2, this.h / 2).draw(svg)
     }
-    this.drawLabel(svg)
+    new Label(this.label, 5).draw(svg)
   }
 }
 
