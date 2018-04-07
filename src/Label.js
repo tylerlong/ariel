b6c5414811ed617ac6aec80dc7a92aafec041d64
Tyler Long <tyler4long@gmail.com>
2018-04-07 09:41:03 +0800

    Support draw4

--- src/Label.js
+++ src/Label.js
@@ -18,7 +18,7 @@ Alignment:
 
 import * as R from 'ramda'
 
-import { lineHeight } from './constants'
+import { lineHeight, labelPadding } from './constants'
 
 class Label {
   constructor (label, alignment = 5) {
@@ -27,6 +27,63 @@ class Label {
   }
 
   draw (svg) {
+    this[`draw${this.alignment}`](svg)
+  }
+
+  draw4 (svg) {
+    const text = svg.append('text').attr('x', labelPadding).attr('y', '50%').attr('fill', 'black').attr('text-anchor', 'start').attr('dominant-baseline', 'central')
+    const lines = R.pipe(
+      R.split('\n'),
+      R.map(R.trim)
+    )(this.label)
+    if (lines.length % 2 === 0) { // even lines, for example: 4
+      let emptyLineHeight = -lineHeight / 2
+      R.forEach(i => {
+        if (R.isEmpty(lines[i])) {
+          emptyLineHeight += lineHeight
+        } else {
+          text.append('tspan').attr('x', labelPadding).attr('dy', -lineHeight - emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
+          emptyLineHeight = 0
+        }
+      }, R.reverse(R.range(0, Math.floor(lines.length / 2))))
+      text.append('tspan').attr('x', labelPadding).attr('y', '50%').attr('alignment-baseline', 'central').attr('visibility', 'hidden').text('.')
+      emptyLineHeight = -lineHeight / 2
+      R.forEach(i => {
+        if (R.isEmpty(lines[i])) {
+          emptyLineHeight += lineHeight
+        } else {
+          text.append('tspan').attr('x', labelPadding).attr('dy', lineHeight + emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
+          emptyLineHeight = 0
+        }
+      }, R.range(Math.floor(lines.length / 2), lines.length))
+    } else { // odd lines, for example: 5
+      let emptyLineHeight = 0
+      R.forEach(i => {
+        if (R.isEmpty(lines[i])) {
+          emptyLineHeight += lineHeight
+        } else {
+          text.append('tspan').attr('x', labelPadding).attr('dy', -lineHeight - emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
+          emptyLineHeight = 0
+        }
+      }, R.reverse(R.range(0, Math.floor(lines.length / 2))))
+      if (R.isEmpty(lines[Math.floor(lines.length / 2)])) {
+        text.append('tspan').attr('x', labelPadding).attr('y', '50%').attr('alignment-baseline', 'central').attr('visibility', 'hidden').text('.')
+      } else {
+        text.append('tspan').attr('x', labelPadding).attr('y', '50%').attr('alignment-baseline', 'central').text(lines[Math.floor(lines.length / 2)])
+      }
+      emptyLineHeight = 0
+      R.forEach(i => {
+        if (R.isEmpty(lines[i])) {
+          emptyLineHeight += lineHeight
+        } else {
+          text.append('tspan').attr('x', labelPadding).attr('dy', lineHeight + emptyLineHeight).attr('alignment-baseline', 'central').text(lines[i])
+          emptyLineHeight = 0
+        }
+      }, R.range(Math.floor(lines.length / 2) + 1, lines.length))
+    }
+  }
+
+  draw5 (svg) {
     const text = svg.append('text').attr('x', '50%').attr('y', '50%').attr('fill', 'black').attr('text-anchor', 'middle').attr('dominant-baseline', 'central')
     const lines = R.pipe(
       R.split('\n'),
--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ export const rectWidth = 200
 export const rectHeight = 120
 export const padding = 16
 export const lineHeight = 20
+export const labelPadding = 20
